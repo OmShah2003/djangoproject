13f5ad9c6ff7a3d412628c2cc2edbb250d83a2c7
om shah <omshahndb2003@gmail.com>
2024-07-01 18:13:06 +0530

    Resolve merge conflicts

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,15 @@ from .views import signup, patient_signup, doctor_signup, patient_dashboard, doc
 from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('signup/patient/', views.patient_signup, name='patient_signup'),
+    path('signup/doctor/', views.doctor_signup, name='doctor_signup'),
+    path('login/', views.user_login, name='login'),
+    path('dashboard/patient/', views.patient_dashboard, name='patient_dashboard'),
+    path('dashboard/doctor/', views.doctor_dashboard, name='doctor_dashboard'),
     path('signup/', signup, name='signup'),
     path('signup/patient/', patient_signup, name='patient_signup'),
     path('signup/doctor/', doctor_signup, name='doctor_signup'),
--- myapp/views.py
+++ myapp/views.py
@@ -35,7 +35,7 @@ def user_profile(request):
     return render(request, 'user_profile.html', context)
 '''
 
-from django.shortcuts import render, redirect
+'''from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import PatientSignUpForm, DoctorSignUpForm
@@ -71,5 +71,54 @@ def patient_dashboard(request):
     return render(request, 'patient_dashboard.html')
 
 @login_required
+def doctor_dashboard(request):
+    return render(request, 'doctor_dashboard.html')
+'''
+# myapp/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import PatientSignUpForm, DoctorSignUpForm
+
+def patient_signup(request):
+    if request.method == 'POST':
+        form = PatientSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = PatientSignUpForm()
+    return render(request, 'signup.html', {'form': form, 'user_type': 'Patient'})
+
+def doctor_signup(request):
+    if request.method == 'POST':
+        form = DoctorSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = DoctorSignUpForm()
+    return render(request, 'signup.html', {'form': form, 'user_type': 'Doctor'})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if user.is_patient:
+                    return redirect('patient_dashboard')
+                elif user.is_doctor:
+                    return redirect('doctor_dashboard')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def patient_dashboard(request):
+    return render(request, 'patient_dashboard.html')
+
 def doctor_dashboard(request):
     return render(request, 'doctor_dashboard.html')
